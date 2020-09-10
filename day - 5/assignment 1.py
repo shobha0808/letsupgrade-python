124910041a63d3c96bbe2ade556e97cc39ce029e
shobha0808 <70837355+shobha0808@users.noreply.github.com>
2020-09-10 21:55:17 +0530

    Update assignment 1.py

--- day - 5/assignment 1.py
+++ day - 5/assignment 1.py
@@ -7,12 +7,12 @@ Original file is located at
     https://colab.research.google.com/drive/1SrN7qghncZ7Aa4CN0AuHyapjEwE4m-WS
 """
 
-one=[1,1,5] 
-two=[1,5,6,4,1,2,3,5]
-if all(a in list_two for a in one):
+list_one=[1,1,5] 
+list_two=[1,5,6,4,1,2,3,5]
+if all(a in list_two for a in list-one):
     print('yeahhh its a match')
 
-one=[1,1,5] 
-two=[1,5,6,5,1,2,3,6]
-if all(a in list_two for a in one):
+list_one=[1,1,5] 
+list_two=[1,5,6,5,1,2,3,6]
+if all(a in list_two for a in list_one):
     print('its gone')
